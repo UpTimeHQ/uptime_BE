1b31bc3e7bb17088fb7666b5e3d9153972c28719
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-11-02 01:34:31 +0100

    Update urls.py

--- uptime/urls.py
+++ uptime/urls.py
@@ -1,14 +1,13 @@
 """
 URL configuration for uptime project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView  # Add this import
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns   # <-- NEW
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -29,10 +28,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', RedirectView.as_view(url='/swagger/', permanent=False)),
     path('admin/', admin.site.urls),
-    path('api/', include('uptimee.urls')),  # App is 'uptimee'
+    path('api/', include('uptimee.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-# Serve media files during development
+# Serve media files (dev only)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# *** NEW *** Serve DRF‑YASG static files in production
+if not settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
